cb991c4e4fc9b2e7d188de43fe9cc27fa3211f98
Junseok <projooni@naver.com>
2021-07-05 18:02:37 +0900

    5장시작

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import MyComponent from "./MyComponent";
+// import MyComponent from "./MyComponent";
+// import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
 
 const App = () => {
-  return <MyComponent name="React~" favoriteNumber={7}>리액트~</MyComponent>;
+  // return <MyComponent name="React~" favoriteNumber={7}>리액트~</MyComponent>;
+  // return <EventPractice />;
+  return <ValidationSample />;
 };
 
 export default App;
